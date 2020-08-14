090f4e5579b64842da04854be85a40a513b637fe
Shachar Itzhaky <corwin.amber@gmail.com>
2020-08-14 17:14:25 +0300

    Refactor: have one instance of "common" with symlinks from volumes.

--- examples/sf/haul-from.sh
+++ examples/sf/haul-from.sh
@@ -4,12 +4,19 @@ D=$1
 
 ok=1
 
+fn=$D/common
+if [ -d $fn ] ; then echo "✓ $fn";
+    rm -rf common
+    cp -R $fn .
+else echo "✗ $fn"; ok=0; fi
+
 for vol in lf plf ; do
     fn=$D/$vol/full
     if [ -d $fn ] ; then echo "✓ $fn"; 
         rm -rf $vol/full
         mkdir -p $vol/full
-        cp -R $fn/{common,*.html} $vol/full/
+        cp -R $fn/*.html $vol/full/
+        (cd $vol/full && ln -s ../../common .)
     else echo "✗ $fn"; ok=0; fi
 done
 
