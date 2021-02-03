d7617538269a5f57afbf4e41a76479e70818e466
Shachar Itzhaky <corwin.amber@gmail.com>
2021-02-03 21:46:18 +0200

    [ci] Bump to 0.12.2-rc1.
    Added a few more lines to CI tests.

--- ci/tests/addons/software-foundations.v
+++ ci/tests/addons/software-foundations.v
@@ -1,3 +1,13 @@
 
 From LF Require Import Basics.
+From LF Require Import Induction.
+From LF Require Import Lists.
+(* From LF Require Import IndProp. *) (* <- stack overflow :( *)
+From LF Require Import Poly.
+
+From PLF Require Import Imp.
+From PLF Require Import HoareAsLogic.
+From PLF Require Import Stlc.
+From PLF Require Import StlcProp.
+
 
